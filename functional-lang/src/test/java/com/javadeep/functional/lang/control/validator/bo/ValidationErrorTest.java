e5b34ada5405c81c9190202d0cfbfa509fff3399
javadeep <jiebaojiebj@gmail.com>
2017-01-10 21:00:04 +0800

    by baojie

--- functional-lang/src/test/java/com/javadeep/functional/lang/control/validator/bo/ValidationErrorTest.java
+++ functional-lang/src/test/java/com/javadeep/functional/lang/control/validator/bo/ValidationErrorTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class ValidationErrorTest {
 
     @Test
-    public void testValidationError() {
+    public void testValidationError_1() {
         ValidationError error = ValidationError.builder("error")
                 .field("field")
                 .errorCode(123)
@@ -19,6 +19,20 @@ public class ValidationErrorTest {
         Assert.assertEquals(123, error.getErrorCode());
         Assert.assertEquals("field", error.getField());
         Assert.assertNull(error.getInvalidValue());
+
+    }
+
+    @Test
+    public void testValidationError_2() {
+        ValidationError error = ValidationError.builder("errors")
+                .field("fields")
+                .errorCode(1234)
+                .invalidValue(3)
+                .build();
+        Assert.assertEquals("errors", error.getErrorMsg());
+        Assert.assertEquals(1234, error.getErrorCode());
+        Assert.assertEquals("fields", error.getField());
+        Assert.assertEquals(3, error.getInvalidValue());
 
     }
 }
